6bf1a708a51f7876d83ad73b44a3c97e124b823c
Ali Baraka <baraka.ali210@gmail.com>
2019-06-10 16:05:42 -0500

    Servlets Exercise completed

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,20 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(urlPatterns = "/test")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+        try {
+            PrintWriter out = res.getWriter();
+            out.println("Working!!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
