60af258f65aa7f7e3051516ccfd7e64eeef76560
Ali Baraka <baraka.ali210@gmail.com>
2019-06-11 12:27:31 -0500

    servlets exercise & jsp and jstl lectures

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,16 +1,17 @@
 import javax.servlet.annotation.WebServlet;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = "/test")
+@WebServlet(urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) {
         try {
             PrintWriter out = res.getWriter();
-            out.println("Working!!");
+            out.println("Hello World!");
         } catch (IOException e) {
             e.printStackTrace();
         }
